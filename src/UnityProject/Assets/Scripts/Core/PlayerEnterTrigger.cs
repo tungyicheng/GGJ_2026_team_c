06b9aaa16ef868d32f5de840eadd30ed2a2f9fbb
Chika Chang <13810485+chikacc@users.noreply.github.com>
2026-02-01 12:40:51 +0800

    feat: add game over panel

---
+++ src/UnityProject/Assets/Scripts/Core/PlayerEnterTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Scenes {
+    public sealed class PlayerEnterTrigger : MonoBehaviour {
+        public event Action OnPlayerEnter;
+
+        void OnTriggerEnter2D(Collider2D other) {
+            if (other.CompareTag("Player")) {
+                OnPlayerEnter?.Invoke();
+            }
+        }
+    }
+}
---
+++ src/UnityProject/Assets/Scripts/Core/TimerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Scenes {
+    public sealed class TimerController : MonoBehaviour {
+        public float ElapsedTime;
+        public bool IsRunning;
+
+        void Update() {
+            if (!IsRunning) return;
+            ElapsedTime += Time.deltaTime;
+        }
+
+        public void ResetTimer() {
+            ElapsedTime = 0;
+        }
+
+        public void StartTimer() {
+            IsRunning = true;
+        }
+
+        public void StopTimer() {
+            IsRunning = false;
+        }
+    }
+}
